19cbbb39cb641ec453874a3843e0e31b0dfd3381
Vinny Oliveira <vinnyolife@users.noreply.github.com>
2026-04-21 17:13:36 +0800

    finish the truncated delay utility function

--- src/utils.js
+++ src/utils.js
@@ -113,9 +113,9 @@ export function resolveElements(target) {
 
 /**
  * Returns a promise that resolves after a given number of milliseconds
- * @param {number} ms
+ * @param {number} ms - delay in milliseconds (defaults to 0 for next tick)
  * @returns {Promise<void>}
  */
-export function delay(ms) {
+export function delay(ms = 0) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
